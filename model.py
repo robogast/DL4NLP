e0ff4f5fbeb7f7aaeaf4cf5bf8377b4cbbee71c0
Mathieu Bartels <mathieubartels@gmail.com>
2020-09-24 15:35:34 +0200

    Create small test network

--- model.py
+++ model.py
@@ -1,21 +1,51 @@
 import torch
+import torch.nn as nn
 import pytorch_lightning as pl
 
 from models.wavenet import WaveNetModel
 
-class LanguageModel(WaveNetModel):
+class LanguageModel(pl.LightningModule):
     def __init__(self):
         super(LanguageModel, self).__init__()
+        bias = True
+        self.convs = nn.Sequential(
+            nn.Conv1d(in_channels=256,
+                out_channels=500,
+                kernel_size=3,
+                bias=bias,
+                padding=2),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=500,
+                out_channels=500,
+                kernel_size=5,
+                bias=bias,
+                padding=3),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=500,
+                out_channels=1,
+                kernel_size=9,
+                bias=bias,
+                padding=2)
+        )
 
-    # def forward(self, data):
-        
-    #     assert False, "TODO: model forward"
+        self.cf = nn.Sequential(
+            nn.Linear(100000, 2)
+        )
+
+        self.lossf = nn.CrossEntropyLoss()
+
+    def forward(self, data):
+        convs = self.convs(data).squeeze(0)
+        out = self.cf(convs)
+        return out
 
     def training_step(self, batch, batch_idx):
         out = self.shared_step(batch, batch_idx)
         print(out)
         # assert False, "TODO: training step"
-        # loss = ...
+        _, label = batch
+        print(label)
+        loss = self.lossf(out, label)
         return pl.TrainResult(minimize=loss)
         # return 
 
@@ -26,9 +56,11 @@ class LanguageModel(WaveNetModel):
 
     def validation_step(self, batch, batch_idx):
         out = self.shared_step(batch, batch_idx)
-        assert False, "TODO: validation step"
-        # loss = ...
-        # return pl.ValidationResult(minimize=loss)
+        _, label = batch
+        print(label)
+        print(out.shape, label.shape)
+        loss = self.lossf(out, label)
+        return pl.EvalResult(loss)
 
     def shared_step(self, batch, batch_idx):
         x, _ = batch
